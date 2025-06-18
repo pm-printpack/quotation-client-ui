382d8ddf39520e9b44237069084d625763ac5a30
Cory <cory@rotender.com>
2025-06-17 23:44:17 -0700

    chore: separate the build dir of prod and stage

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  distDir: process.env.NODE_ENV === "production" ? "prod" : (process.env.NODE_ENV === "test" ? "stage" : "out"),
   trailingSlash: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
