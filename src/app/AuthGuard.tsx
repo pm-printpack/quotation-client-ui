074a9a3abfeb8be67b54a4a48d1903e9c5c57048
Cory <cory@rotender.com>
2025-06-23 18:40:25 -0700

    fix: remove / from the end of paths

--- src/app/AuthGuard.tsx
+++ src/app/AuthGuard.tsx
@@ -29,7 +29,7 @@ export default function AuthGuard({ children }: PropsWithChildren) {
 
   useEffect(() => {
     const token: string | null = localStorage.getItem("jwtToken");
-    const isLoginPage = pathname === "/login/";
+    const isLoginPage = pathname === "/login";
     let isValid: boolean = false;
 
     if (token) {
@@ -46,7 +46,7 @@ export default function AuthGuard({ children }: PropsWithChildren) {
 
     if (!isValid && !isLoginPage) {
       // no valid token and not already on login
-      router.replace("/login/")
+      router.replace("/login");
     } else if (isValid && isLoginPage) {
       // already authenticated but on login page
       router.replace("/");
