5016e4a88540a69c96efe91a1988570e7a31c40d
Cory <cory@rotender.com>
2025-06-23 18:55:10 -0700

    chore: switch out building trailingSlash

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   distDir: process.env.NODE_ENV === "production" ? "out/prod" : (process.env.NODE_ENV === "test" ? "out/stage" : "out/dev"),
-  trailingSlash: true,
+  // trailingSlash: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
